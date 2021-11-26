bb65941750ef4094a783076adf60caa32e7eb9c0
Sanyalak-T <isanyalakt@gmail.com>
2021-11-26 12:23:50 +0700

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-const container = document.querySelector('.container')
-const unsplashURL = 'https://source.unsplash.com/random/'
-const rows = 10 
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
 
-for(let i = 0; i < rows * 3; i++) {
-    const img = document.createElement('img')
-    img.src = `${unsplashURL}${getRandomSize()}`
-    container.appendChild(img)
+const todos = JSON.parse(localStorage.getItem('todos')) 
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
 }
 
-// console.log(getRandomSize())
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-function getRandomSize() {
-    return `${getRandomNr()}x${getRandomNr()}`
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+    if(todoText) {
+        const todoEL = document.createElement('li')
+        if(todo && todo.completed) {
+            todoEL.classList.add('completed')
+        }
+        todoEL.innerHTML = todoText
+
+        todoEL.addEventListener('click', () => {
+            todoEL.classList.toggle('completed')
+            updateLS
+        })
+
+        todoEL.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEL.remove()
+            updateLS
+        })
+
+        todosUL.appendChild(todoEL)
+
+        input.value = ''
+
+        updateLS()
+    }
 }
 
-// console.log(getRandomNr())
+function updateLS() {
+    todosEL = document.querySelectorAll('li')
 
-function getRandomNr() {
-    return Math.floor(Math.random() * 10) + 300
+    const todos = []
+
+    todosEL.forEach(todoEL => {
+        todos.push({
+            text: todoEL.innerText,
+            completed: todoEL.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
